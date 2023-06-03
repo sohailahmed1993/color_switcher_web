386581a97a4c9d8cd3c7f8d9b85d3d32fc5a857a
sohailahmed1993 <sohail1993ahmad@gmail.com>
2023-06-03 21:46:39 +0530

    webPage

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ function changeBG(color){
     document.body.style.backgroundColor = color;
     let txt = document.getElementsByClassName("text");
     console.log(txt);
-    if(color == "black"){
+    if(color == "#000000"){
         for(let elm of txt){
-            elm.style.color = "white"
+            elm.style.color = "#fff"
             console.log(elm)
         }
     }else{
         for(let elm of txt){
             elm.style.color = "black"
-            console.log(elm)
+            console.log(color)
         }
     }
 }
-
+let navbar = document.getElementById("menu-bar")
+window.addEventListener('scroll', ()=>{
+   if(window.pageYOffset >= 300){
+    navbar.classList.add('sticky')
+   }else{
+    navbar.classList.remove('sticky')
+   }
+})
